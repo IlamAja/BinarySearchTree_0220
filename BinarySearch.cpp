b135d900d50874c04643a31a42017479cd3efefc
Ilam Ball <informasiilam@gmail.com>
2024-06-13 15:48:55 +0700

    membuat function inorder

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -67,6 +67,16 @@ class BinaryTree{
 
         void inorder(Node* ptr)
         {
-            
+            if (ROOT == NULL)
+            {
+                cout << "Tree is empty" << endl;
+                return;
+            }
+            if (ptr != NULL)
+            {
+                inorder(ptr->leftchild);
+                cout << ptr->info << " ";
+                inorder(ptr->rightchild);
+            }
         }
 };
