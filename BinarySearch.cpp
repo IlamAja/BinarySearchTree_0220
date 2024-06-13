1a527ef77245f5492c9d4feb7ade87b0699a5d7b
Ilam Ball <informasiilam@gmail.com>
2024-06-13 15:30:18 +0700

    membuat anak kiri dari parent point ke new node

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -41,5 +41,9 @@ class BinaryTree{
                 return; // exit
             }
             
+            if (element < parent->info) // if the value in the data field of the new node is less than that of the
+            {
+                parent->leftchild = newNode; // make the left child of the parent point to the new node
+            }
         }
 };
