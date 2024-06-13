c8dfecea0d7ee820bb640a347a6b19e360dcd171
Ilam Ball <informasiilam@gmail.com>
2024-06-13 16:14:25 +0700

    membuat switch, case dan break

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -127,7 +127,36 @@ int main(){
         cin >> ch;
         cout << endl;
 
-        
+        switch (ch)
+        {
+        case '1':
+        {
+            cout << "Enter a word: ";
+            string word;
+            cin >> word;
+            x.insert(word);
+            break;
+        }
+        case '2':
+        {
+            x.inorder(x.ROOT);
+            break;
+        }
+        case '3':
+        {
+            x.preorder(x.ROOT);
+            break;
+        }
+        case '4':
+        {
+            x.postorder(x.ROOT);
+            break;
+        }
+        default:
+        {
+            cout << "Exit";
+            break;
+        }
+        }
     }
-    
 }
