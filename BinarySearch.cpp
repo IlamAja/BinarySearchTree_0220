a844f3f5e0c4e8838380e4841bc149651fdc1f73
Ilam Ball <informasiilam@gmail.com>
2024-06-13 15:34:16 +0700

    membuat function search dengan parameter string element, node parent, dan node currentNode

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -50,4 +50,9 @@ class BinaryTree{
                 parent->rightchild = newNode; //make the right child of the parent point to the new node
             }
         }
+
+        void search (string element, Node*& parent, Node*& currentNode)
+        {
+            
+        }
 };
